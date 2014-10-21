e9e52c508765d5202a93a2ccb733bad32ec0286f
Pirvu Alice <pirvuflaviaalice@gmail.com>
2014-10-21 22:16:16 +0300

    Exercitii final

--- exercises/Alice-Pirvu/frontdev/scripts/app.js
+++ exercises/Alice-Pirvu/frontdev/scripts/app.js
@@ -31,14 +31,27 @@ var employeesList = [
     }
 ];
 function showList() {
-    var myTable = '<table border="1"><tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Salary</th></tr>';
-    for(var i in employeesList) {
-        myTable +=
-            '<tr><td>'+employeesList[i].firstName+'</td><td>'+employeesList[i].lastName+'</td><td>'+employeesList[i].phone+'</td><td>'+employeesList[i].salary+'</td></tr>';
+
+    unhideList();
+
+    if(employeesList.length>0) {
+        var myTable = '<table border="1" class="list"><tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Salary</th></tr>';
+        for (var i in employeesList) {
+            myTable +=
+                '<tr><td>' + employeesList[i].firstName + '</td><td>' + employeesList[i].lastName + '</td><td>' + employeesList[i].phone + '</td><td>' + employeesList[i].salary + '</td></tr>';
         }
-    myTable += '</table>';
-    var container = document.getElementById('listcontainer');
-    container.innerHTML = myTable;
+        myTable += '</table>';
+        var container = document.getElementById('listcontainer');
+        container.innerHTML = myTable;
+    }
+    else
+    {
+        var text="There are no employess in list.";
+        var container=document.getElementById('listcontainer');
+        container.innerHTML=text;
+    }
+
+
 }
 
 var Employee = function (firstName, lastName, phone, salary) {
@@ -47,31 +60,106 @@ var Employee = function (firstName, lastName, phone, salary) {
     this.phone = phone;
     this.salary = salary;
 }
-function addEmployee() {
+
+function checkEmployee()
+{
+    if(firstName.length==0)
+        return 0;
+
+    if(lastName.length==0)
+        return 0;
+
+    var numbers = /^[0-9]+$/;
+    if(!salary.value.match(numbers))
+        return 0;
+
+    return 1;
+}
+
+function addEmployee()
+{
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
-    var phone = document.getElementById("phone").value; var salary = document.getElementById("salary").value;
-    employeesList.push(new Employee(firstName, lastName, phone,
-        salary));
-   showList();
+    var phone = document.getElementById("phone").value;
+    var salary = document.getElementById("salary").value;
 
+    if(checkEmployee(firstName,lastName,phone,salary)==1)
+        employeesList.push(new Employee(firstName, lastName, phone,salary));
+
+    showList();
+
+    document.getElementById("firstName").value="";
+    document.getElementById("lastName").value="";
+    document.getElementById("phone").value="";
+    document.getElementById("salary").value="";
 }
 
 function SalaryTotal()
 {
-    var sum=0;
+    unhideList();
 
-    for(var i in employeesList)
-        sum+=employeesList[i].salary;
-    var table='<table><tr><td>Total</td><td>'+sum/employeesList.length+'</td></tr></table>';
+    if(employeesList.length>0) {
+        var sum = 0;
 
-    var container = document.getElementById('listcontainer');
-    container.innerHTML = table;
+        var myTable = '<table border="1" class="list"><tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Salary</th></tr>';
+        for (var i in employeesList) {
+            myTable +=
+                '<tr><td>' + employeesList[i].firstName + '</td><td>' + employeesList[i].lastName + '</td><td>' + employeesList[i].phone + '</td><td>' + employeesList[i].salary + '</td></tr>';
+        }
 
+
+        for (var i in employeesList)
+            sum += parseInt(employeesList[i].salary);
+        myTable += '<tr id="total"><td colspan="2">Total</td><td colspan="2">' + (sum / employeesList.length).toFixed() + '</td></tr></table>';
+
+        var container = document.getElementById('listcontainer');
+        container.innerHTML = myTable;
+    }
+
+    else
+    {
+        var text="There are no employess in list.";
+        var container=document.getElementById('listcontainer');
+        container.innerHTML=text;
+    }
 }
 
 function DeleteLastEmployee()
 {
+    unhideList();
 
+    employeesList.pop();
+    if(employeesList.length>0)
+        showList();
+    else
+    {
+        var text="There are no employess in list.";
+        var container=document.getElementById('listcontainer');
+        container.innerHTML=text;
+    }
 }
 
+function OrderBySalary()
+{
+    employeesList.sort(function (a, b) {
+            if (a.salary > b.salary) {
+                return 1;
+            }
+            if (a.salary < b.salary) {
+                return -1;
+            }
+            // a must be equal to b
+            return 0;
+        });
+    showList();
+}
+
+function hideList()
+{
+    var container=document.getElementById('listcontainer').style.visibility='hidden';
+}
+
+function unhideList()
+{
+    var container=document.getElementById('listcontainer').style.visibility='visible';
+}
