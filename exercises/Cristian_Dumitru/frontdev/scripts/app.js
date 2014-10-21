9974504a89f12911bcb3f3383baa0501802f1b71
Cristian Dumitru <criss.dumitru.87@gmail.com>
2014-10-21 12:33:19 +0300

    Workshop1 + exercices

---
+++ exercises/Cristian_Dumitru/frontdev/scripts/app.js
@@ -0,0 +1,73 @@
+/**
+ * Created by Dumitru on 21.10.2014.
+ */
+var employeesList = [
+    {
+        firstName: 'John',
+        lastName: 'King',
+        phone: '0123456789',
+        salary: 123
+    },
+    {
+        firstName: 'Steven',
+        lastName: 'Gerard',
+        phone: '0123456789',
+        salary: 123
+    },
+    {
+        firstName: 'Diana',
+        lastName: 'Ross',
+        phone: '0123456789',
+        salary: 123
+    },
+    {
+        firstName: 'Mike',
+        lastName: 'Bob',
+        phone: '0123456789',
+        salary: 123
+    },
+    {
+        firstName: 'Emily',
+        lastName: 'Hudson',
+        phone: '0123456789',
+        salary: 123
+    }
+];
+
+function showList() {
+    var myTable = '<table class="table" border="1"><tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Salary</th></tr>';
+    for(var i in employeesList) {
+        myTable += '<tr><td>'+employeesList[i].firstName+'</td><td>'+employeesList[i].lastName+'</td><td>'+employeesList[i].phone+'</td><td>'+employeesList[i].salary+'</td></tr>';
+    }
+    myTable += '</table>';
+    var container = document.getElementById('listcontainer');
+    container.innerHTML = myTable;
+}
+
+var Employee = function (firstName, lastName, phone, salary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.salary = salary;
+}
+function addEmployee() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var phone = document.getElementById("phone").value;
+    var salary = document.getElementById("salary").value;
+    employeesList.push(new Employee(firstName, lastName, phone, salary));
+    showList();
+}
+
+function showSalary() {
+    var suma = 0;
+    for(var i in employeesList) {
+        suma += employeesList[i].salary;
+    }
+    document.getElementById('salarycontainer').innerHTML = suma;
+}
+
+function deleteEmployee() {
+    employeesList.pop();
+    showList();
+}
