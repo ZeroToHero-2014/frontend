44573a3227c65112e816f3788aa28f479219e0f9
Antonie Anca <antonie.anca@yahoo.com>
2014-10-23 00:38:31 +0300

    Workshop 3 done
    I have finished

---
+++ excercises/angularhr/scripts/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Anca on 22.10.2014.
+ */
+var hrApp = angular.module('hrApp', ['ngRoute', 'ngResource']);
+hrApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider
+            .when('/contactus', {/*tot ce e trecut in url dupa # nu ajunge la server*/
+                templateUrl: 'templates/something.html',
+                controller: 'FirstController'
+            })
+            .otherwise({/* intra pe orice ruta daca nu gaseste una definita, stabileste ruta default */
+                templateUrl: 'templates/main.html',
+                controller: 'MainController'
+            });
+    }]);
---
+++ excercises/angularhr/scripts/controllers/MenuController.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Anca on 22.10.2014.
+ */
+hrApp.controller('MenuController', ['$scope', function($scope){
+    $scope.currentDate = new Date();
+}]);
