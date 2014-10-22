1e39e1ea758d76048b42aeb1344e99bc0bcf6ea8
Cristian Dumitru <criss.dumitru.87@gmail.com>
2014-10-22 16:48:59 +0300

    fix controller bug

--- exercises/Workshop5/scripts/controllers/DepartmentSearchController.js
+++ exercises/Workshop5/scripts/controllers/DepartmentSearchController.js
@@ -1,7 +1,15 @@
 /**
  * Created by Dumitru on 22.10.2014.
  */
-hrApp.controller('DepartmentSearchController', ['$scope', function($scope) {
+hrApp.controller('DepartmentSearchController', ['$scope', '$http', function($scope, $http) {
     $scope.title = 'Search department!';
-
+    $scope.makeRequest = function() {
+        $scope.rezultat = [];
+        $http = ({url: 'http://demo.teamnet.ro:8282/datamodel/departments/findOne/{inputvalue}', method: GET}).
+            success(function(data, status, headers, config) {
+                $scope.departments = data;
+            }).
+            error(function(data, status, headers, config) {
+            });
+    };
 }]);
