25959aaef325d325b579e2ea08102a6f398b25b0
Cristian Dumitru <criss.dumitru.87@gmail.com>
2014-10-23 17:08:08 +0300

    Add show/hide to Employees List!!

--- exercises/Workshop6/scripts/controllers/EmployeesListController.js
+++ exercises/Workshop6/scripts/controllers/EmployeesListController.js
@@ -4,7 +4,9 @@
 hrApp.controller('EmployeesListController', ['$scope', '$http', function($scope, $http){
     $scope.employees = [];
     $scope.ptButon = [];
+    $scope.myValue = false;
 
+    //get details and show <div> with details
     $scope.getEmployeeDetails = function(value) {
         $http({url: 'http://demo.teamnet.ro:8282/datamodel/employees/findOne/'+value, method: 'GET'})
             .success(function(data, status, headers, config) {
@@ -12,9 +14,19 @@ hrApp.controller('EmployeesListController', ['$scope', '$http', function($scope,
             })
             .error(function(data, status, headers, config) {
             });
-
+        if($scope.myValue == false) {
+            $scope.myValue = true;
+        }
     };
 
+    //hide <div>
+    $scope.hideDetails = function() {
+        if($scope.myValue == true) {
+            $scope.myValue = false;
+        }
+    }
+
+    //get data from server
     $http({url: 'http://demo.teamnet.ro:8282/datamodel/employees/findAll', method: 'GET'})
         .success(function(data, status, headers, config) {
             $scope.employees = data;
