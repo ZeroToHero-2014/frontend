3fcd8757c76bcd5fabc0f0e1e3890294c828f698
Cristian Dumitru <criss.dumitru.87@gmail.com>
2014-10-23 12:51:13 +0300

    Add exercise 2!!

--- exercises/Workshop6/scripts/controllers/EmployeesListController.js
+++ exercises/Workshop6/scripts/controllers/EmployeesListController.js
@@ -3,6 +3,18 @@
  */
 hrApp.controller('EmployeesListController', ['$scope', '$http', function($scope, $http){
     $scope.employees = [];
+    $scope.ptButon = [];
+
+    $scope.getEmployeeDetails = function(value) {
+        $http({url: 'http://demo.teamnet.ro:8282/datamodel/employees/findOne/'+value, method: 'GET'})
+            .success(function(data, status, headers, config) {
+                $scope.ptButon = data;
+            })
+            .error(function(data, status, headers, config) {
+            });
+
+    };
+
     $http({url: 'http://demo.teamnet.ro:8282/datamodel/employees/findAll', method: 'GET'})
         .success(function(data, status, headers, config) {
             $scope.employees = data;
