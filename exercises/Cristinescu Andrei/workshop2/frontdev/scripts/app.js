42cfc6bf0711d8472b89681f1822a789128ca056
Andrei G <c_ristinescu@yahoo.com>
2014-10-23 14:34:04 +0300

    workshops

---
+++ exercises/Cristinescu Andrei/workshop2/frontdev/scripts/app.js
@@ -0,0 +1,76 @@
+/**
+ * Created by Andrei on 10/21/2014.
+ */
+var employeesList = [
+    {
+        firstName: 'John',
+        lastName: 'King',
+        phone: '0123456789',
+        salary: 700
+    },
+    {
+        firstName: 'Steven',
+        lastName: 'Gerard',
+        phone: '0123456789',
+        salary: 7900
+    },
+    {
+        firstName: 'Diana',
+        lastName: 'Ross',
+        phone: '0123456789',
+        salary: 67464
+    },
+    {
+        firstName: 'Mike',
+        lastName: 'Bob',
+        phone: '0123456789',
+        salary: 3453
+    },
+    {
+        firstName: 'Emily',
+        lastName: 'Hudson',
+        phone: '0123456789',
+        salary: 5654
+    }
+];
+function showList() {
+    var myTable = '<div id="myTable"><table border="1" class="table table-hover" ><tr><th>First Name</th><th>Last Name</th><th>Phone</th><th>Salary</th></tr>';
+    for(var i in employeesList) {
+        myTable += '<tr><td>'+employeesList[i].firstName+'</td><td>'+employeesList[i].lastName+'</td><td>'+employeesList[i].phone+'</td><td>'+employeesList[i].salary+'</td></tr>';
+    }
+    myTable += '</table> </div>';
+    var container = document.getElementById('listcontainer');
+    s = document.getElementsByClassName("SumB");
+
+
+}
+function sum(){
+    var s =0;
+   for(var i=0; i<employeesList.length;i++){
+           s += employeesList[i].salary;
+   }
+    var totalSalary = '<p>Total sum of salaries: ' +s + '<\p>';
+    var mySum = document.getElementById("suma");
+
+    suma.innerHTML = totalSalary;
+}
+function remove(){
+
+    console.log("am ajuns aici");
+    employeesList.pop();
+    showList();
+}
+var Employee = function (firstName, lastName, phone, salary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.salary = salary;
+}
+function addEmployee(){
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var phone = document.getElementById("phone").value;
+    var salary = document.getElementById("salary").value;
+    employeesList.push(new Employee(firstName, lastName, phone,salary));
+    showList();
+}
