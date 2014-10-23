6fb33bce7a05e43c28c1a974c75fab67f853e8c7
sMoKeSz <iuly_zep_pelin@yahoo.com>
2014-10-23 11:10:18 +0300

    project 2.3
    add 5(4,5)

--- exercises/angularhr/scripts/controllers/DepartmentSearchController.js
+++ exercises/angularhr/scripts/controllers/DepartmentSearchController.js
@@ -1,5 +1,12 @@
 hrApp.controller('DepartmentSearchController', ['$scope', function($scope) {
     $scope.title = 'Department Search';
     $scope.descriptionShow = false;
-
+    $scope.search = function() {
+        for (var department in departments)
+        if ($scope.depID==department.departmentId) {
+            answer.departmentId = department.departmentId;
+            answer.departmentName = department.departmentName;
+            answer.locationId.city = department.locationId.city;
+        }
+    }
 }]);
