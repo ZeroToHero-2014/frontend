68a3e405ceea82d217cb63a6695400e4b62d9186
Cristian Dumitru <criss.dumitru.87@gmail.com>
2014-10-23 19:18:52 +0300

    Add exercise 3!!

--- exercises/Workshop6/scripts/controllers/EmployeeAddController.js
+++ exercises/Workshop6/scripts/controllers/EmployeeAddController.js
@@ -15,11 +15,19 @@ hrApp.controller('EmployeeAddController', ['$scope', '$http', function ($scope, 
         success(function (data) {
             $scope.jobs = data;
         });
+
+    //variable to show/hide the error
+    $scope.errorValue = false;
     /**
      * Reset form
      */
     $scope.reset = function () {
         this.employee = {};
+
+        //hide the error
+        if($scope.errorValue == true) {
+         $scope.errorValue = false;
+         }
     };
     /**
      * Persist an employee
@@ -27,9 +35,18 @@ hrApp.controller('EmployeeAddController', ['$scope', '$http', function ($scope, 
      */
     $scope.create = function (addEmployee) {
         console.log(addEmployee);
-        $http({url: 'http://demo.teamnet.ro:8282/datamodel/employees/create', method: 'PUT',data:addEmployee}).
-            success(function (data) {
+
+        $http({url: 'http://demo.teamnet.ro:8282/datamodel/employees/create', method: 'PUT',data:addEmployee})
+            .success(function (data) {
                 $scope.employee = data;
+            })
+            .error(function (data) {
+                $scope.error = "The command hasn't been execute, please try again!";
+
+                //show the error
+                if($scope.errorValue == false) {
+                    $scope.errorValue = true;
+                }
             });
     }
 }]);
