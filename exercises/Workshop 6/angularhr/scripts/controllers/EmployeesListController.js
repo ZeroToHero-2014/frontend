7a4ae6fa116e068c12b0fcb36ca5942137f56d24
Ifrim Giani <giani.ifrim@gmail.com>
2014-10-23 14:10:58 +0300

    Workshop 6 - AngularJS Forms
    Workshop 6 - AngularJS Forms - exercise 1 & exercise 2

--- exercises/Workshop 6/angularhr/scripts/controllers/EmployeesListController.js
+++ exercises/Workshop 6/angularhr/scripts/controllers/EmployeesListController.js
@@ -1,5 +1,6 @@
 hrApp.controller('EmployeesListController', ['$scope', '$http', function($scope, $http){
     $scope.employees = [];
+    $scope.details =[];
     $http({url: 'http://demo.teamnet.ro:8282/datamodel/employees/findAll', method: 'GET'}).
         success(function(data, status, headers, config) {
             $scope.employees = data;
@@ -7,4 +8,15 @@ hrApp.controller('EmployeesListController', ['$scope', '$http', function($scope,
         }).
         error(function(data, status, headers, config) {
         });
+
+    $scope.cauta = function(idangajat) {
+        $http({url: 'http://demo.teamnet.ro:8282/datamodel/employees/findOne/'+idangajat , method: 'GET'}).
+            success(function (data, status, headers, config) {
+                $scope.details = data;
+                 console.log(data);
+            }).
+            error(function (data, status, headers, config) {
+            });
+    }
+
 }]);
