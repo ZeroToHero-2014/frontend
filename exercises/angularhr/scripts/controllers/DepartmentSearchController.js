f82353e9928b56f5b9190a3be99f748499d7a8d7
sMoKeSz <iuly_zep_pelin@yahoo.com>
2014-10-23 15:40:14 +0300

    project 2.4
    add workshop 3 4 5 complet

--- exercises/angularhr/scripts/controllers/DepartmentSearchController.js
+++ exercises/angularhr/scripts/controllers/DepartmentSearchController.js
@@ -1,12 +1,33 @@
-hrApp.controller('DepartmentSearchController', ['$scope', function($scope) {
+hrApp.controller('DepartmentSearchController', ['$scope', '$http', function($scope, $http){
+    $scope.departments = [];
     $scope.title = 'Department Search';
-    $scope.descriptionShow = false;
+    $scope.show=false;
+    $scope.show1=false;
+    $http({url: 'http://demo.teamnet.ro:8282/datamodel/departments/findAll', method: 'GET'}).
+        success(function(data, status, headers, config) {
+            $scope.departments = data;
+        }).
+        error(function(data, status, headers, config) {
+        });
+
+
+
+
+
+
+
     $scope.search = function() {
-        for (var department in departments)
-        if ($scope.depID==department.departmentId) {
-            answer.departmentId = department.departmentId;
-            answer.departmentName = department.departmentName;
-            answer.locationId.city = department.locationId.city;
+    var aux=0;
+        for (department in $scope.departments) {
+            if ($scope.depID == $scope.departments[department].departmentId) {
+                $scope.departmentId = $scope.departments[department].departmentId;
+                $scope.departmentName = $scope.departments[department].departmentName;
+                $scope.city = $scope.departments[department].locationId.city;
+                aux=1;
+            }
+
         }
+       if(aux==1) {$scope.show=true; $scope.show1=false;}
+            else if(aux==0) $scope.show1=true;
     }
 }]);
