680e9920b77fd996355e6f18366802640246b4be
ovi ovi <craiu.ovidiu93@yahoo.com>
2014-10-24 19:59:23 +0300

    Workshop 7(date+validation on edit)

--- exercises/hrapplication/scripts/controllers/MenuController.js
+++ exercises/hrapplication/scripts/controllers/MenuController.js
@@ -4,4 +4,5 @@
 hrApp.controller('MenuController', ['$scope', 'employeeActionsService',
     function($scope, employeeActionsService){
         $scope.employeeActionList = employeeActionsService;
+        $scope.currentDate=new Date();
     }]);
