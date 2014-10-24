2a780541ff4a399a7253deaacbe8f3171c1b4c5e
anateodoragrigore <anna730punct@yahoo.com>
2014-10-24 12:56:22 +0300

    work 7

---
+++ exercises/hrapplication/scripts/app.js
@@ -0,0 +1,28 @@
+var hrApp = angular.module('hrApp', ['ngRoute', 'ngResource']);
+    hrApp.config(['$routeProvider',function($routeProvider) {
+        $routeProvider
+    .when('/employeelist', {
+        templateUrl: 'templates/employeelist.html',
+        controller: 'EmployeeListController'
+    })
+    .when('/employeeadd', {
+        templateUrl: 'templates/employeeadd.html',
+        controller: 'EmployeeAddController'
+    })
+    .when('/employeeview/:employeeid', {
+        templateUrl: 'templates/employeeview.html',
+        controller: 'EmployeeViewController'
+    })
+    .when('/employeeedit/:employeeid', {
+        templateUrl: 'templates/employeeedit.html',
+        controller: 'EmployeeEditController'
+    })
+    .when('/employeedelete/:employeeid', {
+        templateUrl: 'templates/employeedelete.html',
+        controller: 'EmployeeDeleteController'
+    })
+    .otherwise({
+        templateUrl: 'templates/main.html',
+        controller: 'MainController'
+    });
+    }]);
---
+++ exercises/hrapplication/scripts/services/MenuServices.js
@@ -0,0 +1,11 @@
+hrApp.value('employeeActionsService', [
+        {
+            label: "List employees",
+            url: "#/employeelist"
+        },
+        {
+            label: "Add employee",
+            url: "#/employeeadd"
+        }
+    ]
+);
