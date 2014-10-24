c795eefcec94597735663475ae293d184591d4cc
AndreeaMaria <andreeutza_1991@yahoo.com>
2014-10-24 16:40:10 +0300

    Workshop 7 (fara exercitii)

---
+++ exercises/hrapplication/scripts/controllers/MainController.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Maria on 10/24/2014.
+ */
+
+hrApp.controller('MainController', ['$scope', function($scope) {
+    $scope.title = 'HR Application';
+    $scope.description = "Application for managing employees";
+}]);
---
+++ exercises/hrapplication/scripts/services/MenuServices.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Maria on 10/24/2014.
+ */
+
+hrApp.value('employeeActionsService', [
+        {
+            label: "List employees",
+            url: "#/employeeslist"
+        },
+        {
+            label: "Add employee",
+            url: "#/employeeadd"
+        }
+    ]
+);
