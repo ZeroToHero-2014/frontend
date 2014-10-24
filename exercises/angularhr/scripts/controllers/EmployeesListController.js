1a69f85736b1ac441061c765b864aa0b5e0c0827
agricib <agricibogdan@gmail.com>
2014-10-24 15:18:54 +0300

    rev wrks 6

--- exercises/angularhr/scripts/controllers/EmployeesListController.js
+++ exercises/angularhr/scripts/controllers/EmployeesListController.js
@@ -6,23 +6,20 @@ hrApp.controller('EmployeesListController', ['$scope', '$http', function($scope,
 
     $scope.show=false;
 
-
-    $http({url: 'http://demo.teamnet.ro:8282/datamodel/employees/findAll', method: 'GET'})
-        .success(function(data, status, headers, config) {
+    $http({url: 'http://demo.teamnet.ro:8282/datamodel/employees/findAll/', method: 'GET'})
+        .success(function (data, status, headers, config) {
             $scope.employees = data;
         })
-        .error(function(data, status, headers, config) {
+        .error(function (data, status, headers, config) {
         });
 
-        $scope.action = function(employee) {
-        $scope.id=employee.employeeId;
-        $scope.firstName=employee.firstName;
-        $scope.lastName=employee.lastName;
-        $scope.salary=employee.salary;
-        $scope.departmentName=employee.departmentId.departmentName;
-        $scope.email=employee.email;
-        $scope.phone=employee.phoneNumber;
-        $scope.city=employee.departmentId.locationId.city;
-        $scope.show=true;
-   }
+    $scope.action = function(employee) {console.log(employee)
+        $http({url: 'http://demo.teamnet.ro:8282/datamodel/employees/findOne/' +employee.employeeId, method: 'GET'})
+            .success(function (data, status, headers, config) {
+                $scope.employee = data;
+                $scope.show = true;
+            })
+            .error(function (data, status, headers, config) {
+            });
+    }
 }]);
