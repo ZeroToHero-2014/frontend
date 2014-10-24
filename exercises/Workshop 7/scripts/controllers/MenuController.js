6dadb10a1e953fb5c8e45c5f74b6e3206e0409d6
Ifrim Giani <giani.ifrim@gmail.com>
2014-10-24 13:00:42 +0300

    Workshop 7 - AngularJS HR Application
    Workshop 7 - AngularJS HR Application + exercises

---
+++ exercises/Workshop 7/scripts/controllers/MenuController.js
@@ -0,0 +1,4 @@
+hrApp.controller('MenuController', ['$scope', 'employeeActionsService', function($scope, employeeActionsService){
+    $scope.employeeActionList = employeeActionsService;
+    $scope.currentDate = new Date();
+}]);
