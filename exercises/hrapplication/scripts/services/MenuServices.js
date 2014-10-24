12709a334d331cdcd9d4cd523172a49939cdc900
Daniel <daniel_ioan90@yahoo.com>
2014-10-24 18:07:01 +0300

    aplicatie hr

---
+++ exercises/hrapplication/scripts/services/MenuServices.js
@@ -0,0 +1,13 @@
+hrApp.value('employeeActionsService', [
+        {
+            label: "List employees",
+            url: "#/employeeslist"
+        },
+        {
+            label: "Add employee",
+            url: "#/employeeadd"
+        }
+    ]
+);/**
+ * Created by user on 10/24/2014.
+ */
